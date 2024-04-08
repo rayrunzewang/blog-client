22dc6277c212f9a0891b268bfae9a8a602bb532f
Ray Wang <rayrunzewang@gmail.com>
2024-04-08 14:29:43 +1000

    refactor/abstract Loading component in tag list

--- src/app/(dashboard)/(subject)/layout.js
+++ src/app/(dashboard)/(subject)/layout.js
@@ -1,14 +1,15 @@
 import styles from './layout.module.css';
 import { Suspense } from 'react';
 import { TagUnorderedList } from '@/components/TagUnorderedList';
+import { Loading } from '@/components/Loading';
 
 const SubjectLayout = ({ children }) => {
   return (
 
     <div>
       <div className={styles.tagManager}>
-        <Suspense fallback={<div className={styles.loading}>Loading...</div>}>
-          <TagUnorderedList></TagUnorderedList>
+        <Suspense fallback={<Loading />}>
+          <TagUnorderedList />
         </Suspense>
       </div>
       {children}
