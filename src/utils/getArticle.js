4df0a1777d7d03e9de1c7117b813bf3907af1b2f
Ray Wang <rayrunzewang@gmail.com>
2024-04-13 15:17:29 +1000

    feat/add revalidate to fetch

--- src/utils/getArticle.js
+++ src/utils/getArticle.js
@@ -1,9 +1,7 @@
 export async function getArticle(id) {
   const data = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/learning/article/${id}`, {
-    headers: {
-      'Cache-Control': 'no-cache' 
-  }
-});
+    next: { revalidate: 10 }, 
+  });
 
   // if (!data.ok) {
   //   throw new Error('Failed to fetch data');
