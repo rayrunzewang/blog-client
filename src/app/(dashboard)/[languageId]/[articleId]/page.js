f05c97e3e805683e8bc2d5da6722ed581134c319
Ray Wang <rayrunzewang@gmail.com>
2024-04-14 20:57:56 +1000

    feat/add article lock, edit, delete button

--- src/app/(dashboard)/[languageId]/[articleId]/page.js
+++ src/app/(dashboard)/[languageId]/[articleId]/page.js
@@ -1,22 +1,45 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import { getArticle } from '@/utils/getArticle';
 import { LoadingText } from '@/components/loadingUi/LoadingText';
 import styles from './page.module.css'
+import { CiLock } from "react-icons/ci";
+import { CiUnlock } from "react-icons/ci";
+
 import useSWR from 'swr';
 
 export default function page({ params: { languageId, articleId } }) {
+  const [isArticleLocked, setIsArticleLocked] = useState(true)
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
   const { data: article, error, mutate } = useSWR(`${process.env.NEXT_PUBLIC_API_BASE_URL}/learning/article/${articleId}`, fetcher);
+
+  const handleLockClick = () => {
+    setIsArticleLocked(!isArticleLocked)
+  }
 
   // const article = await getArticle(articleId);
   return (
     <ul>
       {article ? (
         <div>
-          <p className={styles.title}>{article.title}</p>
-          <p className={styles.body}>{article.body}</p>
+          <ul className={styles.EditToolUL}>
+            <li
+              className={`${styles.EditToolList} ${styles.EditToolListLock}`}
+              onClick={handleLockClick}
+            >{isArticleLocked ? <CiLock /> : <CiUnlock />}</li>
+   
+                <li className={`${styles.EditToolList} ${isArticleLocked && styles.EditToolListHidden}`}>Edit</li>
+
+                <li className={`${styles.EditToolList} ${isArticleLocked && styles.EditToolListHidden}`}>Delete</li>
+          </ul>
+          <article>
+            <p className={styles.title}>{article.title}</p>
+            <p className={styles.paragraph}>{article.body}</p>
+            {/* {article.body.map((blogContent , index)=>{
+              <p key={index}>{blogContent.content}</p>
+            })} */}
+          </article>
         </div>
       ) : (
         <LoadingText number={10} />
